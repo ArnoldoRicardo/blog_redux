6670e174e7c84ccd9feddebc1acaf1a3f6288d41
ArnoldoRicardo <anyback@gmail.com>
2021-01-03 22:39:31 -0600

    update api url

--- src/actions/publicacionActions.js
+++ src/actions/publicacionActions.js
@@ -5,27 +5,36 @@ import axios from 'axios';
 import { TRAER_PUBLICACION, CARGANDO, ERROR } from '../types/publicacionTypes';
 
 export const traerTodos = () => async (dispatch) => {
+  dispatch({
+    type: CARGANDO,
+  });
+  try {
+    // const publicaciones = await axios.get('http://localhost:3001/posts');
+    const publicaciones = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+
     dispatch({
-        type: CARGANDO,
+      type: TRAER_PUBLICACION,
+      payload: publicaciones.data,
     });
-    try {
-        const publicaciones = await axios.get('http://localhost:3001/posts');
-        dispatch({
-            type: TRAER_PUBLICACION,
-            payload: publicaciones.data,
-        });
-    } catch (error) {
-        dispatch({
-            type: ERROR,
-            payload: 'Algo salio mal intente mas tarde',
-        });
-    }
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: 'Algo salio mal intente mas tarde',
+    });
+  }
 };
 
 export const traerPorUsuario = (key) => async (dispatch) => {
-    const publicaciones = await axios.get(`http://localhost:3001/posts?usuario=${key}`);
-    dispatch({
-        type: TRAER_PUBLICACION,
-        payload: publicaciones.data,
-    });
+  //   const publicaciones = await axios.get(
+  //     `http://localhost:3001/posts?usuario=${key}`
+  //   );
+  const publicaciones = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts?usuarioId=${key}`
+  );
+  dispatch({
+    type: TRAER_PUBLICACION,
+    payload: publicaciones.data,
+  });
 };
--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -5,20 +5,22 @@ import axios from 'axios';
 import { TRAER_USUARIOS, CARGANDO, ERROR } from '../types/usuariosTypes';
 
 export const traerTodos = () => async (dispatch) => {
+  dispatch({
+    type: CARGANDO,
+  });
+  try {
+    const usuarios = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    // const usuarios = await axios.get('http://localhost:3001/users');
     dispatch({
-        type: CARGANDO,
+      type: TRAER_USUARIOS,
+      payload: usuarios.data,
     });
-    try {
-        // const usuarios = await axios.get('https://jsonplaceholder.typicode.com/users');
-        const usuarios = await axios.get('http://localhost:3001/users');
-        dispatch({
-            type: TRAER_USUARIOS,
-            payload: usuarios.data,
-        });
-    } catch (error) {
-        dispatch({
-            type: ERROR,
-            payload: 'Algo salio mal intente mas tarde',
-        });
-    }
+  } catch (error) {
+    dispatch({
+      type: ERROR,
+      payload: 'Algo salio mal intente mas tarde',
+    });
+  }
 };
