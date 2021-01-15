58787e13f6aef55da075c65d29e5c29bd87ae023
ArnoldoRicardo <anyback@gmail.com>
2021-01-15 00:19:20 -0600

    mostar publicaciones

--- src/actions/publicacionActions.js
+++ src/actions/publicacionActions.js
@@ -5,26 +5,26 @@ import axios from 'axios';
 import { TRAER_POR_USUARIO, CARGANDO, ERROR } from '../types/publicacionTypes';
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
-  const { publicaciones } = getState().publicacionReducer;
-
-  dispatch({
-    type: CARGANDO,
-  });
-
-  try {
-    // const response = await axios.get(`http://localhost:3001/posts?usuario=${key}`);
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${key}`);
-
-    const publicacionesActualizadas = { ...publicaciones, [key]: response.data };
+    const { publicaciones } = getState().publicacionReducer;
 
     dispatch({
-      type: TRAER_POR_USUARIO,
-      payload: publicacionesActualizadas,
+        type: CARGANDO,
     });
-  } catch (error) {
-    dispatch({
-      type: ERROR,
-      payload: 'Algo salio mal intente mas tarde',
-    });
-  }
+
+    try {
+        // const response = await axios.get(`http://localhost:3001/posts?usuario=${key}`);
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${key}`);
+
+        const publicacionesActualizadas = { ...publicaciones, [key]: response.data };
+
+        dispatch({
+            type: TRAER_POR_USUARIO,
+            payload: publicacionesActualizadas,
+        });
+    } catch (error) {
+        dispatch({
+            type: ERROR,
+            payload: 'Algo salio mal intente mas tarde',
+        });
+    }
 };
--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -2,6 +2,8 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Spinner from '../General/Spinner';
+import Fatal from '../General/Fatal';
 
 import * as usuariosActions from '../../actions/usuariosActions';
 import * as publicacionActions from '../../actions/publicacionActions';
@@ -10,42 +12,100 @@ const { traerTodos: usuariosTraerTodos } = usuariosActions;
 const { traerPorUsuario: publicacionTraerPorUsuario } = publicacionActions;
 
 class Publicaciones extends Component {
-  constructor() {
-    super();
-    this.state = {
-      usuario: null,
-    };
-  }
-  async componentDidMount() {
-    if (!this.props.usuarioReducer.usuarios.length) {
-      await this.props.usuariosTraerTodos();
+    constructor() {
+        super();
+        this.state = {
+            usuario: null,
+        };
     }
-    if (!this.props.publicacionReducer.publicaciones.hasOwnProperty(this.props.match.params.key)) {
-      this.props.publicacionTraerPorUsuario(this.props.match.params.key);
-    }
-  }
+    async componentDidMount() {
+        const {
+            usuariosTraerTodos,
+            publicacionTraerPorUsuario,
+            match: {
+                params: { key },
+            },
+        } = this.props;
 
-  render() {
-    console.log(this.props.publicacionReducer.publicaciones);
-    return (
-      <div>
-        <h1>Publicaciones de </h1>
-        {this.props.match.params.key}
-      </div>
-    );
-  }
+        if (!this.props.usuarioReducer.usuarios.length) {
+            await usuariosTraerTodos();
+        }
+        if (!this.props.publicacionReducer.publicaciones.hasOwnProperty(key)) {
+            publicacionTraerPorUsuario(key);
+        }
+    }
+
+    ponerUsuario = () => {
+        const {
+            usuarioReducer,
+            match: {
+                params: { key },
+            },
+        } = this.props;
+
+        if (usuarioReducer.error) {
+            return <Fatal mesaje={usuarioReducer.error} />;
+        }
+
+        if (!usuarioReducer.usuarios.length || usuarioReducer.cargando) {
+            return <Spinner />;
+        }
+
+        const nombre = usuarioReducer.usuarios[key].name;
+
+        return (
+            <div>
+                <h1>Publicaciones de {nombre} </h1>
+            </div>
+        );
+    };
+
+    ponerPublicaciones = () => {
+        const {
+            publicacionReducer,
+            publicacionReducer: { publicaciones },
+            match: {
+                params: { key },
+            },
+        } = this.props;
+
+        if (publicacionReducer.error) {
+            return <Fatal mesaje={publicacionReducer.error} />;
+        }
+
+        if (!publicaciones.hasOwnProperty(key) || publicacionReducer.cargando) {
+            return <Spinner />;
+        }
+
+        return publicaciones[key].map((publicacion) => (
+            <div className="pub_titulo" key={publicacion.id}>
+                <h2>{publicacion.title}</h2>
+                <p>{publicacion.body}</p>
+            </div>
+        ));
+    };
+
+    render() {
+        console.log(this.props.publicacionReducer);
+        return (
+            <div>
+                {this.ponerUsuario()}
+                {this.ponerPublicaciones()}
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = ({ usuarioReducer, publicacionReducer }) => {
-  return {
-    usuarioReducer,
-    publicacionReducer,
-  };
+    return {
+        usuarioReducer,
+        publicacionReducer,
+    };
 };
 
 const mapDispatchToProps = {
-  usuariosTraerTodos,
-  publicacionTraerPorUsuario,
+    usuariosTraerTodos,
+    publicacionTraerPorUsuario,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones);
