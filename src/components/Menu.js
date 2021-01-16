7b29578e29b0efaccc9b07f03fd21f29ff515934
ArnoldoRicardo <anyback@gmail.com>
2021-01-15 19:28:51 -0600

    deploy

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Menu() {
-  return (
-    <nav id="menu">
-      <Link to="/">Usuarios</Link>
-      <Link to="/tareas">Tareas</Link>
-    </nav>
-  );
+    return (
+        <nav id="menu">
+            <Link to="/blog_redux">Usuarios</Link>
+            <Link to="/tareas">Tareas</Link>
+        </nav>
+    );
 }
